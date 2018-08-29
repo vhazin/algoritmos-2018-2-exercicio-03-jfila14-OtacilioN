5d3c107b70d1be1c1abc60dfa82e38e3cdf5dc58
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-08-29 19:44:07 -0300

    Now alternative.c is the main version
    By logic the main version become alternative.c

--- alternative.c
+++ alternative.c
@@ -1,109 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Node
-{
-    int id;
-    struct Node *prox;
-} node;
-
-typedef struct Fila
-{
-    struct Node *inicial;
-    struct Node *final;
-} fila;
-
-node *aloca(int id);
-void inicia(fila *FILA, int idInicial);
-void insere(fila *FILA, int id);
-void exibe(fila *FILA);
-void retira(fila *FILA, int id);
 
 int main(void)
 {
-    fila *FILA = (fila *)malloc(sizeof(fila));
     unsigned int n;
-    unsigned int id;
     scanf("%d", &n);
-    scanf("%d", &id);
-    inicia(FILA, id);
-    n--;
-    while (n--)
+    unsigned int nFila[n];
+    for (unsigned int x = 0; x < n; x++)
+        scanf("%d", &nFila[x]);
+
+    unsigned int m;
+    scanf("%d", &m);
+    unsigned int mFila[m];
+    for (unsigned int x = 0; x < m; x++)
+        scanf("%d", &mFila[x]);
+
+    for (unsigned int x = 0; x < n; x++)
     {
-        scanf("%d", &id);
-        insere(FILA, id);
-    }
-
-    scanf("%d", &n);
-    while (n--)
-    {
-        scanf("%d", &id);
-        retira(FILA, id);
-    }
-    exibe(FILA);
-    printf("\n");
-    return 0;
-}
-
-node *aloca(int id)
-{
-    node *novo = (node *)malloc(sizeof(node));
-    novo->id = id;
-    return novo;
-}
-
-void inicia(fila *FILA, int idInicial)
-{
-    node *novo = aloca(idInicial);
-    novo->prox = NULL;
-    FILA->inicial = novo;
-    FILA->final = novo;
-}
-
-void insere(fila *FILA, int id)
-{
-    node *novo = aloca(id);
-    novo->prox = NULL;
-
-    node *tmp = FILA->final;
-    tmp->prox = novo;
-    FILA->final = novo;
-}
-
-void exibe(fila *FILA)
-{
-
-    node *tmp;
-    tmp = FILA->inicial;
-    while (tmp != NULL)
-    {
-        printf("%d ", tmp->id);
-        tmp = tmp->prox;
-    }
-}
-
-void retira(fila *FILA, int id)
-{
-    node *tmp = FILA->inicial;
-    node *anterior = NULL;
-    while (tmp != NULL)
-    {
-        if (tmp->id == id)
-        {
-            if (!anterior)
+        char flag = 1;
+        for (unsigned int y = 0; y < m; y++)
+            if (nFila[x] == mFila[y])
             {
-                FILA->inicial = tmp->prox;
+                flag = 0;
                 break;
             }
-            if (tmp->prox == NULL)
-                FILA->final = NULL;
-
-            anterior->prox = tmp->prox;
-            break;
-        }
-        anterior = tmp;
-        tmp = tmp->prox;
+        if (flag)
+            printf("%d ", nFila[x]);
     }
+    printf("\n");
 
-    free(tmp);
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,32 +1,109 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int id;
+    struct Node *prox;
+} node;
+
+typedef struct Fila
+{
+    struct Node *inicial;
+    struct Node *final;
+} fila;
+
+node *aloca(int id);
+void inicia(fila *FILA, int idInicial);
+void insere(fila *FILA, int id);
+void exibe(fila *FILA);
+void retira(fila *FILA, int id);
 
 int main(void)
 {
+    fila *FILA = (fila *)malloc(sizeof(fila));
     unsigned int n;
+    unsigned int id;
     scanf("%d", &n);
-    unsigned int nFila[n];
-    for (unsigned int x = 0; x < n; x++)
-        scanf("%d", &nFila[x]);
-
-    unsigned int m;
-    scanf("%d", &m);
-    unsigned int mFila[m];
-    for (unsigned int x = 0; x < m; x++)
-        scanf("%d", &mFila[x]);
-
-    for (unsigned int x = 0; x < n; x++)
+    scanf("%d", &id);
+    inicia(FILA, id);
+    n--;
+    while (n--)
     {
-        char flag = 1;
-        for (unsigned int y = 0; y < m; y++)
-            if (nFila[x] == mFila[y])
+        scanf("%d", &id);
+        insere(FILA, id);
+    }
+
+    scanf("%d", &n);
+    while (n--)
+    {
+        scanf("%d", &id);
+        retira(FILA, id);
+    }
+    exibe(FILA);
+    printf("\n");
+    return 0;
+}
+
+node *aloca(int id)
+{
+    node *novo = (node *)malloc(sizeof(node));
+    novo->id = id;
+    return novo;
+}
+
+void inicia(fila *FILA, int idInicial)
+{
+    node *novo = aloca(idInicial);
+    novo->prox = NULL;
+    FILA->inicial = novo;
+    FILA->final = novo;
+}
+
+void insere(fila *FILA, int id)
+{
+    node *novo = aloca(id);
+    novo->prox = NULL;
+
+    node *tmp = FILA->final;
+    tmp->prox = novo;
+    FILA->final = novo;
+}
+
+void exibe(fila *FILA)
+{
+
+    node *tmp;
+    tmp = FILA->inicial;
+    while (tmp != NULL)
+    {
+        printf("%d ", tmp->id);
+        tmp = tmp->prox;
+    }
+}
+
+void retira(fila *FILA, int id)
+{
+    node *tmp = FILA->inicial;
+    node *anterior = NULL;
+    while (tmp != NULL)
+    {
+        if (tmp->id == id)
+        {
+            if (!anterior)
             {
-                flag = 0;
+                FILA->inicial = tmp->prox;
                 break;
             }
-        if (flag)
-            printf("%d ", nFila[x]);
-    }
-    printf("\n");
+            if (tmp->prox == NULL)
+                FILA->final = NULL;
 
-    return 0;
+            anterior->prox = tmp->prox;
+            break;
+        }
+        anterior = tmp;
+        tmp = tmp->prox;
+    }
+
+    free(tmp);
 }
